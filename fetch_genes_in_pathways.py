9578d4324fcbe69b03971757bda871f35be6be6e
Tuo Zhang <taz2008@med.cornell.edu>
2018-12-13 14:32:15 -0500

    create a python script for extracting genes in selected pathways (for Aram's project)

---
+++ fetch_genes_in_pathways.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 13 14:30:16 2018
+
+@author: taz2008
+"""
+
+# fetch_genes_in_pathways.py
+# given a set of pathways, fetch genes in each pathway
+# 
+
+
